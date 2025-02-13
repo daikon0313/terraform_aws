6d576de8e7d4b3f2efbf1aed0fc16e9a4574013e
daisuke harato <d.harato@datumstudio.jp>
2025-02-13 17:47:08 +0900

    first commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 ############################################
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"VPCのCIDRブロック
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
